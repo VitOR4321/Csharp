add42e81d812a3139fb44ad18172f8f1abb0563c
VitOR4321 <92822828+VitOR4321@users.noreply.github.com>
2023-01-02 09:27:24 +0100

    Add files via upload

---
+++ instruction 9/zadanie 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace zadanie_1
+{
+    class Program
+    {
+        class Temperatura
+        {
+            public double Celcius;
+        }
+        static void Main(string[] args)
+        {
+            Temperatura Temp1 = new Temperatura();
+            Temperatura Temp2 = new Temperatura();
+            Temp1.Celcius = 36.6;
+            Temp2.Celcius = Temp1.Celcius - 1;
+            Console.Write("Temp2 wynosi: " + Temp2.Celcius);
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ instruction 9/zadanie 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace zadanie_3
+{
+    class Program
+    {
+        class Temperatura
+        {
+            private double celcious;
+
+            public double Celcious
+            {
+                set { celcious = value; }
+                get { return celcious; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Temperatura Temp1 = new Temperatura();
+            Temperatura Temp2 = new Temperatura();
+
+            Temp1.Celcious = 36.6;
+            Temp2.Celcious = 34.7;
+
+            Console.Write("Temp1 wynosi: " + Temp1.Celcious +"\n");
+            Console.Write("Temp2 wynosi: " + Temp2.Celcious + "\n");
+
+            double zmiana=Temp1.Celcious;
+            Temp1.Celcious = Temp2.Celcious;
+            Temp2.Celcious = zmiana;
+
+            Console.Write("Po zmianie\n");
+            Console.Write("Temp1 wynosi: " + Temp1.Celcious + "\n");
+            Console.Write("Temp2 wynosi: " + Temp2.Celcious + "\n");
+            Console.ReadKey(true);
+        }
+    }
+}
