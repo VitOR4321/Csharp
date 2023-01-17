6c9300c0a7d9fc5b9dbee4e2621c3a4ac08e90dd
VitOR4321 <92822828+VitOR4321@users.noreply.github.com>
2023-01-18 00:39:41 +0100

    Add files via upload

---
+++ Recruitment/DisplayApp/Program.cs
@@ -0,0 +1,33 @@
+
+using DisplayApp.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<ContactService>();
+builder.Services.AddHttpClient<IContactService, ContactService>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7243");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Recruitment/Services/IContactService.cs
@@ -0,0 +1,17 @@
+﻿using ContactsListApp.Models;
+
+namespace DisplayApp.Services
+{
+    public interface IContactService
+    {
+        Task<List<Contact>> GetContacts();
+
+        Task<Contact> GetContact(Guid id);
+
+        Task<Contact> DeleteContact(Guid id);
+
+        Task<UpdateContactRequest> EditContact(Guid id);
+
+        Task<AddContactRequest> AddContact();
+    }
+}
